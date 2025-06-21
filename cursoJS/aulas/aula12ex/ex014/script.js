d4218f52e9b04bd94d9d43e51e27b5d588da6a13
b1960ahia <jbahia38@gmail.com>
2025-06-21 18:36:52 -0300

    upgrad

--- cursoJS/aulas/aula12ex/ex014/script.js
+++ cursoJS/aulas/aula12ex/ex014/script.js
@@ -4,7 +4,7 @@ function carregar(){
     var img = document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 13
+    var hora = 8
     msg.innerHTML = `Agora são ${hora} horas`
     if( hora > 0 && hora < 12){
         img.src = 'manha.png'
