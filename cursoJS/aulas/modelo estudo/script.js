07e65dec45e6d340a369586ebd5a1dad91739816
b1960ahia <jbahia38@gmail.com>
2025-06-24 20:09:30 -0300

    atual

--- cursoJS/aulas/modelo estudo/script.js
+++ cursoJS/aulas/modelo estudo/script.js
@@ -1,1 +1,11 @@
-alert ('Olá!');
+let numCarros = 6;
+let todosCarrosMesmaMarca = false;
+
+if(numCarrros === 5 || todosCarrosMesmaMarca ===true) {
+    console.log('Ou todos os carros são da mesma marca');
+    console.log('Ou temos cinco carros');
+    console.log('As duas opções juntas');
+    else{
+    console.log('Nao temos cinco carros nem todos são da mesma marca');
+    }
+}
