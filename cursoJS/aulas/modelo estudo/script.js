c2c2b6ac8b0693ae239cbf8f88f3e419c0d3d797
b1960ahia <jbahia38@gmail.com>
2025-06-24 20:49:23 -0300

    modelo estudo

--- cursoJS/aulas/modelo estudo/script.js
+++ cursoJS/aulas/modelo estudo/script.js
@@ -1,11 +1,12 @@
-let numCarros = 6;
-let todosCarrosMesmaMarca = false;
+let nome = 'Joao';
+let idade = 65;
+let solteiro = true;
 
-if(numCarrros === 5 || todosCarrosMesmaMarca ===true) {
-    console.log('Ou todos os carros são da mesma marca');
-    console.log('Ou temos cinco carros');
-    console.log('As duas opções juntas');
-    else{
-    console.log('Nao temos cinco carros nem todos são da mesma marca');
+if ( solteiro === false) {
+    console.log( `${nome} esta solteiro!`);
+   
+}else {
+    console.log( `${nome} esta casado!`);
     }
-}
+
+//pag 25
