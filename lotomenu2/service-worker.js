8613c58e1f00ca4ab4d985f5b70da270214541ea
b1960ahia <jbahia38@gmail.com>
2025-10-29 17:04:25 -0300

    lomenu2analise

---
+++ lotomenu2/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'loterias-cache-v1';
+const urlsToCache = [
+  '/JavaScript/LotoMenu/',
+  '/JavaScript/LotoMenu/lotomenu.html',
+  '/JavaScript/LotoMenu/manifest.json',
+  '/JavaScript/LotoMenu/icone-192.png',
+  '/JavaScript/LotoMenu/icone-512.png'
+  // acrescente outros arquivos JS/CSS que usar
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
+
