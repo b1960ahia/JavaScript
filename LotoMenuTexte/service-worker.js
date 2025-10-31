a52cbf35ce8a65364dd863ffc3b8084310e5cbc5
b1960ahia <jbahia38@gmail.com>
2025-10-31 17:43:21 -0300

    parececorrigido

--- LotoMenuTexte/service-worker.js
+++ LotoMenuTexte/service-worker.js
@@ -1,25 +1,33 @@
 const CACHE_NAME = 'loterias-cache-v1';
 const urlsToCache = [
-  '/JavaScript/LotoMenu/',
-  '/JavaScript/LotoMenu/lotomenu.html',
-  '/JavaScript/LotoMenu/manifest.json',
-  '/JavaScript/LotoMenu/icone-192.png',
-  '/JavaScript/LotoMenu/icone-512.png'
-  // acrescente outros arquivos JS/CSS que usar
+  './',
+  './analisededados.html',
+  './manifest.json',
+  './icone-192.png',
+  './icone-512.png'
+  // Se tiver CSS/JS externos locais, adicione aqui, ex: './app.css', './app.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
+      )
+    )
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
